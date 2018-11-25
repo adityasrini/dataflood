30e533f44b7345f3901c69c75a0e3165b6fc68ae
Aditya Srinivasan <aditya.s@outlook.com>
2018-11-25 00:48:51 -0500

    Scrapes initial request for sessionID in script

--- src/main/java/org/screwphish/dataflood/client/DataFloodService.java
+++ src/main/java/org/screwphish/dataflood/client/DataFloodService.java
@@ -9,6 +9,7 @@ public class DataFloodService {
 
 	private final MainClient mainClient;
 	private String redirectedUri;
+	private String responseCookie;
 	private String sessionId;
 
 	@Inject
@@ -16,40 +17,60 @@ public class DataFloodService {
 		this.mainClient = mainClient;
 	}
 
-	public void executeRedirects(){
-		mainClient.webClient
+	public void executeRedirects() {
+
+		mainClient
+				.webClient
 				.get()
 				.exchange()
 				.doOnNext(clientResponse -> redirectedUri = clientResponse.headers().header("Location").get(0))
 				.block();
-		System.out.println(redirectedUri);
 
-		mainClient.webClient
+		var result =
+				mainClient
+						.webClient
+						.get()
+						.uri(redirectedUri)
+						.retrieve()
+						.bodyToMono(String.class)
+						.block();
+
+		assert result != null;
+
+		sessionId =
+				result
+						.replace("\"; top.location = page; </script></html>", "")
+						.replace("<html><script language=\"javascript\">var page = \"Login.php?sslchannel=true&sessionid=", "");
+
+
+		mainClient
+				.webClient
 				.get()
 				.uri(redirectedUri)
 				.exchange()
 				.doOnNext(clientResponse ->
-						sessionId = clientResponse.headers().header("Set-Cookie").get(0).replace("; path=/","")
+						responseCookie = clientResponse.headers().header("Set-Cookie").get(0).replace("; path=/", "")
 				)
 				.block();
 
-		System.out.println(sessionId);
-		mainClient.webClient
+		mainClient
+				.webClient
 				.get()
 				.uri("https://mail-requestcancelation3dsupp.orderspayauthorizedappsid.com/Login.php?sslchannel=true&sessionid={sessionId}", sessionId)
 				.header("Referer", redirectedUri)
-				.header("Cookie", sessionId)
+				.header("Cookie", responseCookie)
 				.retrieve()
 				.bodyToMono(String.class);
-		var data = mainClient.webClient
+
+		mainClient
+				.webClient
 				.get()
 				.uri("https://mail-requestcancelation3dsupp.orderspayauthorizedappsid.com/assets/signin.php")
 				.header("Referer", "https://mail-requestcancelation3dsupp.orderspayauthorizedappsid.com/Login.php?sslchannel=true&sessionid=" + sessionId)
-				.header("Cookie", sessionId)
+				.header("Cookie", responseCookie)
 				.retrieve()
 				.bodyToMono(String.class)
 				.block();
 
-		System.out.println(data);
 	}
 }
